c865ecb0dbc590af1295de5daad6d3796d2422dc
nayan97 <nayanislam085@gmail.com>
2023-09-17 12:15:23 +0600

    admin template added

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index(){
+        return view('admin.pages.dashboard');
+    }
+
+    public function login(){
+        return view('admin.pages.login');
+    }
+}
